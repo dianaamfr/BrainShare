06e74bf651b713c289621993def82d063f7b2557
Juliane Marubayashi <up201800175@fe.up.pt>
2021-02-27 23:32:26 +0000

    Added courses to the homepage

--- src/html/pages/homepage.php
+++ src/html/pages/homepage.php
@@ -34,27 +34,37 @@ include_once(dirname(__DIR__) . "\common\header.php");
 <hr>
 
 <div class="container mt-5 feature">
+
+    <h1 class="text-center my-5">Courses</h1>
     <div class="row">
-        <div class="col-lg-4 feature-courses">
+        <div class="col-lg-4 feature-courses my-5">
             <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
+                <title>MIEIC</title>
                 <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
             </svg>
             <h2 class="mt-4">MIEIC</h2>
+            <p>Matesters in Informatics and Computation Engineering</p>
+            <a href="#" class="btn btn-outline-primary">Check questions</a>
         </div>
-        <div class="col-lg-4 feature-courses">
+        <div class="col-lg-4 feature-courses my-5">
+            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <title>MIEEC</title>
+                <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+            </svg>
+            <h2 class="mt-4">MIEC</h2>
+            <p>Masters in Civil Engineering</p>
+            <a href="#" class="btn btn-outline-primary">Check questions</a>
+        </div>
+        <div class="col-lg-4 feature-courses my-5">
             <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
                 <title>Placeholder</title>
                 <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
             </svg>
-        </div>
-        <div class="col-lg-4 feature-courses">
-            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
-            </svg>
+            <h2 class="mt-4">MIEEC</h2>
+            <p>Master in Electrical and Computers Engineering</p>
+            <a href="#" class="btn btn-outline-primary">Check questions</a>
         </div>
     </div>
-
-
 </div>
+
+<hr> 
