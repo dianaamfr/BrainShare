c4dba8fbe9e6de5f0d8573def5cd7972eb858e95
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2021-02-27 11:17:59 +0000

    Initial bootstrap configuration

---
+++ html/pages/homepage.php
@@ -0,0 +1,4 @@
+<h1>Hello, world!</h1>
+
+<button type="button" class="btn btn-primary">Primary</button>
+
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+  include_once('html/common/header.php');
+  include_once('html/pages/homepage.php');
+  include_once('html/common/footer.php');
+?>
