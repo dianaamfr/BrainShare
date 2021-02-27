9c82aed6c6b24149e35442279b90d64374a48b8f
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2021-02-27 16:33:16 +0000

    added hero text and second-feature

--- src/html/pages/homepage.php
+++ src/html/pages/homepage.php
@@ -1,4 +1,59 @@
-<h1>BrainShare</h1>
+<?php
 
-<button type="button" class="btn btn-primary">Primary</button>
+include_once(dirname(__DIR__) . "\common\header.php");
+?>
 
+<div class="position-relative overflow-hidden p-5 text-center bg-light">
+    <div class="col-md-5 mx-auto">
+        <h1 class="mb-4 fw-bold title-homepage"> BrainShare </h1>
+        <p class="lead fw-normal"> The FEUP website for questions and answers. We connect students to solutions to improve your academic grow. </p>
+        <div class="d-flex justify-content-center">
+            <a href="#" class="btn btn-outline-primary mx-2">Search</a>
+            <a href="#" class="btn btn-outline-primary mx-2">Add Question</a>
+        </div>
+    </div>
+
+</div>
+
+
+<div class="row my-5">
+    <div class="offset-md-1 col-md-6 my-5">
+        <h2>What is BrainShare?</h2>
+        <p class="lead">Brainshare is a Q&A forum that allows FEUP students to answer their own doubts about about their courses!!
+            All our features are completely free, since our goal is to be the center of information in our college</p>
+        <a href="#" class="btn btn-outline-primary">About Us</a>
+    </div>
+    <div class="col-md-5">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
+        </svg>
+    </div>
+</div>
+<hr>
+
+<div class="container mt-5 feature">
+    <div class="row">
+        <div class="col-lg-4 feature-courses">
+            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+            </svg>
+            <h2 class="mt-4">MIEIC</h2>
+        </div>
+        <div class="col-lg-4 feature-courses">
+            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+            </svg>
+        </div>
+        <div class="col-lg-4 feature-courses">
+            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+            </svg>
+        </div>
+    </div>
+
+
+</div>
