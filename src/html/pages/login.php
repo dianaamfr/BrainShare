ff1a0280e928ab545978aa7ca4a719ce0c9c9002
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2021-02-27 22:39:27 +0000

    Start register and login pages

---
+++ src/html/pages/login.php
@@ -0,0 +1,22 @@
+<div class="login-background container-fluid main_container ">
+    <div class="card shadow-lg p-3 mb-5 bg-light text-dark rounded position-absolute top-50 start-50 translate-middle">
+        <h3 class="mb-3">Login</h3>
+        <form>
+        <div class="mb-3">
+            <label class="form-label">Email address</label>
+            <input type="email" class="form-control" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Password</label>
+            <input type="password" class="form-control">
+        </div>
+        <a href="#" class="link-dark">Forgot Password</a>
+        <button type="submit" class="btn btn-primary btn-block">Login</button>
+        <button type="submit" class="btn btn-primary btn-block">Register</button>
+        </form>
+        
+    </div> 
+</div>
+
+
+
---
+++ src/html/pages/register.php
@@ -0,0 +1,31 @@
+<div class="register-background container-fluid main_container ">
+    <div class="card shadow-lg p-3 mb-5 bg-light text-black rounded position-absolute top-50 start-50 translate-middle">
+        <h3 class="mb-3">Register</h3>
+        <form>
+        <div class="col-lg-4">
+            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="#777"></rect>
+            <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+        </svg>
+      </div>
+        <div class="mb-3">
+            <label class="form-label">Username</label>
+            <input type="email" class="form-control" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Email address</label>
+            <input type="email" class="form-control" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Password</label>
+            <input type="password" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Repeat Password</label>
+            <input type="password" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary btn-block">Register</button>
+        </form>
+    </div> 
+</div>
