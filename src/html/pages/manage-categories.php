f87bc2c49f64aa3bf6535af5ff0ebf2f2cb3528d
Juliane Marubayashi <up201800175@fe.up.pt>
2021-02-28 11:05:21 +0000

    Added some tags

---
+++ src/html/pages/manage-categories.php
@@ -0,0 +1,60 @@
+<?php
+
+include_once(dirname(__DIR__) . "\common\header.php");
+
+
+?>
+
+<link href="../../css/styles.css" rel="stylesheet">
+<div class="manage-hero text-center bg-light p-3">
+    <h1 class="display-4 mt-5">Manage Categories</h1>
+    <p class="lead my-3">Add or remove tags and courses</p>
+</div>
+<div class="d-flex mx-2 my-3">
+    <!-- Manage tags-->
+    <div class="p-2 bg-light p-4 w-100">
+        <h2 class="display-5 mb-5">Tags</h2>
+        <div class="container">
+            <!-- tags here -->
+            <div class="row">
+                <div class="card mt-3 mx-1 col-md-4">
+                    <div class="card-body manage-tag-card">
+                        <span>PPIN</span>
+                        <a href="#" class="manage-tags-trash"></a>
+                    </div>
+                </div>
+                <div class="card mt-3 mx-1 col-md-4">
+                    <div class="card-body manage-tag-card">MF II</div>
+                </div>
+                <div class="card mt-3 mx-1 col-md-4">
+                    <div class="card-body manage-tag-card">Exam</div>
+                </div>
+                <div class="card mt-3 mx-1 col-md-4">
+                    <div class="card-body manage-tag-card">COMP</div>
+                </div>
+                <div class="card mt-3 mx-1 col-md-4">
+                    <div class="card-body manage-tag-card">IART</div>
+                </div>
+                <div class="card mt-3 mx-1 col-md-4">
+                    <div class="card-body manage-tag-card">IART</div>
+                </div>
+                <div class="card mt-3 mx-1 col-md-4">
+                    <div class="card-body manage-tag-card">Python</div>
+                </div>
+
+
+
+
+
+            </div>
+
+
+        </div>
+    </div>
+
+    <!-- Manage courses -->
+    <div class="p-2 bg-light-blue mx-2 p-4 w-100">
+        <h2 class="display-5">Courses</h2>
+
+    </div>
+</div>
