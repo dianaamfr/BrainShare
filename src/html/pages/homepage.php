d5d5a02537d749a790e335e4fd4e3abf7bda6518
Juliane Marubayashi <up201800175@fe.up.pt>
2021-02-28 19:39:15 +0000

    Layout about done

--- src/html/pages/homepage.php
+++ src/html/pages/homepage.php
@@ -1,7 +1,7 @@
 <?php
 
 include_once(dirname(__DIR__) . "\common\header.php");
-?> 
+?>
 
 <link href="../../css/styles.css" rel="stylesheet">
 <div class="position-relative overflow-hidden p-5 text-center align-middle homepage-container-hero text-light">
@@ -22,17 +22,21 @@ include_once(dirname(__DIR__) . "\common\header.php");
 <div class="mx-3">
     <!-- Featured text  -->
     <div class="row py-5">
-        <div class="offset-md-1 col-md-6 feature-text">
-            <h1 class="mb-4 ">What is BrainShare? <span class="text-muted ">The way to learn.</span> </h1>
-            <p class="lead mb-5">Brainshare is a Q&A forum that allows FEUP students to answer their own doubts about about their courses!
-                All our features are completely free, since our goal is to be the center of information in our college.</p>
-            <a href="#" class="btn btn-outline-primary mb-4">About Us</a>
-        </div>
-        <div class="col-md-5 feature-image">
-            <svg class="img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
-            </svg>
+
+        <div class="row py-5 flex-d justify-content-center align-items-center">
+            <div class=" offset-md-1 col-md-6 feature-text">
+                <h1 class="mb-4 ">What is BrainShare? <span class="text-muted ">The way to learn.</span> </h1>
+                <p class="lead mb-5">Brainshare is a Q&A forum that allows FEUP students to answer their own doubts about about their courses!
+                    All our features are completely free, since our goal is to be the center of information in our college.</p>
+
+                <a href="#" class="btn btn-outline-primary mb-4">About Us</a>
+            </div>
+            <div class="col-md-5 feature-image ">
+                <svg class="img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
+                </svg>
+            </div>
         </div>
 
 
