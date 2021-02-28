ab62c3b9f0eb3a75594c5cfe2753fea3e9845798
Diana Freitas <up201806230@fe.up.pt>
2021-02-28 11:57:46 +0000

    Breakpoint corrections; HTML/CSS validation

--- src/html/common/header.php
+++ src/html/common/header.php
@@ -30,7 +30,7 @@
         </a>
 
         <!-- Mobile Menu Icon -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="fasse" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>      
 
