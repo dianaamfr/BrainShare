782e87fbf37f1638493c4e68634b7a25a32a6bda
Juliane Marubayashi <up201800175@fe.up.pt>
2021-02-28 00:29:29 +0000

    Responsive feature text

--- src/html/pages/homepage.php
+++ src/html/pages/homepage.php
@@ -5,7 +5,7 @@ include_once(dirname(__DIR__) . "\common\header.php");
 
 <div class="position-relative overflow-hidden p-5 text-center bg-light">
     <div class="col-md-5 mx-auto">
-        <h1 class="mb-4 fw-bold title-homepage"> BrainShare </h1>
+        <h1 class="display-3 mb-4"> BrainShare </h1>
         <p class="lead fw-normal"> The FEUP website for questions and answers. We connect students to solutions to improve your academic grow. </p>
         <div class="d-flex justify-content-center">
             <a href="#" class="btn btn-outline-primary mx-2">Search</a>
@@ -18,14 +18,14 @@ include_once(dirname(__DIR__) . "\common\header.php");
 
 
 <div class="row my-5">
-    <div class="offset-md-1 col-md-6 my-5">
-        <h2>What is BrainShare?</h2>
-        <p class="lead">Brainshare is a Q&A forum that allows FEUP students to answer their own doubts about about their courses!!
-            All our features are completely free, since our goal is to be the center of information in our college</p>
+    <div class="offset-md-1 col-md-6 homepage-feature-text">
+        <h1 class="mb-4 ">What is BrainShare? <span class="text-muted ">The way to learn.</span> </h1>
+        <p class="lead mb-5">Brainshare is a Q&A forum that allows FEUP students to answer their own doubts about about their courses!
+            All our features are completely free, since our goal is to be the center of information in our college.</p>
         <a href="#" class="btn btn-outline-primary">About Us</a>
     </div>
-    <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+    <div class="col-md-5 ">
+        <svg class="homepage-feature-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
             <title>Placeholder</title>
             <rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
         </svg>
@@ -33,11 +33,10 @@ include_once(dirname(__DIR__) . "\common\header.php");
 </div>
 <hr>
 
-<div class="container mt-5 feature">
-
-    <h1 class="text-center my-5">Courses</h1>
+<div class="container mt-5">
+    <h1 class="text-center my-5 display-5">Courses</h1>
     <div class="row">
-        <div class="col-lg-4 feature-courses my-5">
+        <div class="col-lg-4 homepage-feature-courses my-5">
             <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
                 <title>MIEIC</title>
                 <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
@@ -46,7 +45,7 @@ include_once(dirname(__DIR__) . "\common\header.php");
             <p>Matesters in Informatics and Computation Engineering</p>
             <a href="#" class="btn btn-outline-primary">Check questions</a>
         </div>
-        <div class="col-lg-4 feature-courses my-5">
+        <div class="col-lg-4 homepage-feature-courses my-5">
             <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
                 <title>MIEEC</title>
                 <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
@@ -55,7 +54,7 @@ include_once(dirname(__DIR__) . "\common\header.php");
             <p>Masters in Civil Engineering</p>
             <a href="#" class="btn btn-outline-primary">Check questions</a>
         </div>
-        <div class="col-lg-4 feature-courses my-5">
+        <div class="col-lg-4 homepage-feature-courses my-5">
             <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
                 <title>Placeholder</title>
                 <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
@@ -66,5 +65,12 @@ include_once(dirname(__DIR__) . "\common\header.php");
         </div>
     </div>
 </div>
-
-<hr> 
+<hr>
+<div class="homepage-container-lastsearch my-5 d-flex mx-auto flex-column text-center">
+    <h1 class="display-5 mb-3">Looking for questions to answer?</h1>
+    <p class="lead mb-5">Don't forget that other students are also needing your help!
+        Learning-by-teaching is one of our lemmas. By explaining and sharing your knowledge you're able to increase
+        your comprehension in a topic. Show what you have learned and don't forget to have fun!
+    </p>
+    <p><a href="#" class="btn btn-outline-primary mb-5">Search</a></p>
+</div>
