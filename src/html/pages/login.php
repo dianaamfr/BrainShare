2442e13bbe949f6dfc8249c282f63d42ff11f43b
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2021-03-01 12:38:34 +0000

    Finish login page. HTML validated.

--- src/html/pages/login.php
+++ src/html/pages/login.php
@@ -1,21 +1,19 @@
-<div class="login-background container-fluid main_container ">
-    <div class="card shadow-lg p-3 mb-5 bg-light text-dark rounded position-absolute top-50 start-50 translate-middle">
+<div id="login">
+    <section class="shadow-lg p-3 mb-5 bg-light text-dark rounded  position-absolute top-50 start-50 translate-middle">
         <h3 class="mb-3">Login</h3>
-        <form>
-        <div class="mb-3">
-            <label class="form-label">Email address</label>
-            <input type="email" class="form-control" placeholder="name@example.com">
-        </div>
-        <div class="mb-3">
-            <label class="form-label">Password</label>
-            <input type="password" class="form-control">
-        </div>
-        <a href="#" class="link-dark">Forgot Password</a>
-        <button type="submit" class="btn btn-primary btn-block">Login</button>
-        <button type="submit" class="btn btn-primary btn-block">Register</button>
+        <form class="form-login">
+            <div class="mb-3">
+                <input type="email" class="form-control" placeholder="Email">
+            </div>
+            <div class="mb-3">
+                <input type="password" class="form-control" placeholder="Password">
+            </div>
+            <button type="submit" class="btn btn-primary btn-login">Login</button>
+
         </form>
-        
-    </div> 
+        <button type="submit" class="btn btn-primary btn-register">Register</button>
+        <a href="#" class="forgot-password justify-content-center">Forgot Password</a>
+    </section>
 </div>
 
 
