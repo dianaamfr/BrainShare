f487f313a78bf10c9e7e77f7bd8f4ce476568922
up201806490 <up201806490@fe.up.pt>
2021-03-02 15:07:13 +0000

    Got cards from Diana

--- src/html/common/header.php
+++ src/html/common/header.php
@@ -7,6 +7,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link href="css/styles.css" rel="stylesheet">
+    <link href="css/search.css" rel="stylesheet">
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous" defer></script>
     <script src="https://kit.fontawesome.com/cf05cece41.js" crossorigin="anonymous"></script>
@@ -15,4 +16,7 @@
   </head>
 
 <body>
-  <main> 
+  <main>
+  <div>
+    confirming header  
+  </div>
