6012adf5916b7f2de4293a9a58276062a2c1cdfe
up201806490 <up201806490@fe.up.pt>
2021-03-02 21:27:58 +0000

    Html done

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
   include_once('html/common/header.php');
-  include_once('html/pages/homepage.php');
+  include_once('html/pages/user-management.php');
   include_once('html/common/footer.php');
 ?>
