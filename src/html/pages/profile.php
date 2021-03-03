6a8d14921707295b439b6bbffdbb0da4d78379d8
Diogo Samuel Fernandes <up201806250@fe.up.pt>
2021-03-03 09:17:15 +0000

    Profile grid responsive

--- src/html/pages/profile.php
+++ src/html/pages/profile.php
@@ -1,22 +1,22 @@
 <section id="profile-main" class="grid-profile container mt-5">
     <div class="one">
         <h3 class="nickname my-4"><b>Joaquina123</b></h3>
-        <div class="profile-pic col">
+        <div class="profile-pic col-lg">
             <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice">
                 <rect width="100%" height="100%" fill="#777"></rect>
             </svg>
         </div>
     </div>
     <div class="two">
-        <div class="row my-4">
-            <section class="profile-info col">
+        <div class="row mt-4">
+            <section class="profile-info col-lg">
                 <h3>Personal Information</h3>
                 <p><i class="fas fa-user"></i> Name: Maria Joaquina</p>
                 <p><i class="fas fa-at"></i> E-mail: up201806230@fe.up.pt</p>
                 <p><i class="fa fa-calendar" aria-hidden="true"></i> Birthday: 09/02/1997</p>
             </section>
 
-            <section class="profile-about-me col">
+            <section class="profile-about-me col-lg">
                 <h3>About Me</h3>
                 <p>Hello! It's Joaquina! I'm currently studying Informatic Engineering (MIEIC) at FEUP. My dream is to work at google and become a great software engineer. I love dogs and cats, I hope I love you too.</p>
             </section>
@@ -28,14 +28,16 @@
             <p><i class="fas fa-graduation-cap"></i> Course: MIEIC</p>
             <p><i class="fa fa-calendar" aria-hidden="true"></i> Current year: 3rd</p>
             <div>
-                <span class="category tag badge bg-secondary"> 
-                    <i class="fas fa-hashtag"></i>
-                    MIPS
-                </span>
-                <span class="category tag badge bg-secondary">
-                    <i class="fas fa-hashtag"></i>
-                    COMP
-                </span>
+                <p><i class="fas fa-tags"></i> Tags:
+                    <span class="category tag badge bg-secondary"> 
+                        <i class="fas fa-hashtag"></i>
+                        MIPS
+                    </span>
+                    <span class="category tag badge bg-secondary">
+                        <i class="fas fa-hashtag"></i>
+                        COMP
+                    </span>
+                </p>
             </div>
         </section>
     </div>
