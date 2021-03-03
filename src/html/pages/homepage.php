65b71f353d30daac571cd086ac0033bd5b9e010e
Diana Freitas <up201806230@fe.up.pt>
2021-03-03 02:19:10 +0000

    Background and Font sizes

--- src/html/pages/homepage.php
+++ src/html/pages/homepage.php
@@ -3,83 +3,83 @@
 include_once(dirname(__DIR__) . "\common\header.php");
 ?> 
 
-<link href="../../css/styles.css" rel="stylesheet">
-<div class="position-relative p-5 text-center homepage-container-hero text-light d-flex justify-content-center flex-column">
-    <div class="banner-text col-md-5 mx-auto">
-        <h1 class="display-3"> BrainShare </h1>
-        <p class="lead fw-normal mb-3"> The FEUP website for questions and answers. We connect students to solutions to improve your academic grow. </p>
-        <div class="d-flex justify-content-center">
-            <a href="#" class="btn btn-primary mx-2">Search</a>
-            <a href="#" class="btn btn-primary mx-2">Add Question</a>
+<div class="position-relative text-center homepage-container-hero text-light">
+        <div class="banner-text col-md-5 mx-auto">
+            <h1 class="display-3"> BrainShare </h1>
+            <p class="lead fw-normal mb-3"> The FEUP website for questions and answers. We connect students to solutions to improve your academic grow. </p>
+            <div class="d-flex justify-content-center">
+                <a href="#" class="btn btn-primary mx-3">Search</a>
+                <a href="#" class="btn btn-primary mx-3">Add Question</a>
+            </div>
         </div>
+
     </div>
 
-</div>
+<div class="page-margin">
 
-<!-- title->top-margin: 5; bottom-margin: 4-->
-<!-- text-> bottom-margin: 5 -->
-
-<div class="mx-3">
-    <!-- Featured text  -->
-    <div class="row py-5 homepage-feature">
-        <div class="offset-md-1 col-md-6 homepage-feature-text">
-            <h1 class="mb-4 ">What is BrainShare? <span class="text-muted ">The way to learn.</span> </h1>
-            <p class="lead mb-5">Brainshare is a Q&A forum that allows FEUP students to answer their own doubts about about their courses!
-                All our features are completely free, since our goal is to be the center of information in our college.</p>
-            <a href="#" class="btn btn-outline-primary mb-4">About Us</a>
-        </div>
-        <div class="col-md-5 homepage-feature-image">
-            <svg class="img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
-            </svg>
-        </div>
-
-
-    </div>
-    <hr>
-    <!-- Courses -->
-    <div class="pt-3 homepage-course">
-        <h1 class="text-center my-5 display-5 fw-normal">Courses</h1>
-        <div class="row">
-            <div class="col-lg-4 homepage-course-container my-5">
-                <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <title>MIEIC</title>
-                    <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
-                </svg>
-                <h2 class="mt-4">MIEIC</h2>
-                <p>Matesters in Informatics and Computation Engineering</p>
-                <a href="#" class="btn btn-outline-primary">Check questions</a>
+    <div class="mx-3">
+        <!-- Featured text  -->
+        <div class="row py-5 homepage-feature">
+            <div class="offset-md-1 col-md-6 homepage-feature-text">
+                <h2 class="mb-4 ">What is BrainShare?  <span class="text-muted ">The way to learn.</span> </h2>
+                <p class="mb-5">BrainShare is a Q&A forum that allows FEUP students to answer their own doubts about about their courses!
+                    All our features are completely free, since our goal is to be the center of information in our college.</p>
+                <a href="#" class="btn btn-secondary mb-4">About Us</a>
             </div>
-            <div class="col-lg-4 homepage-course-container my-5">
-                <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <title>MIEEC</title>
-                    <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
-                </svg>
-                <h2 class="mt-4">MIEC</h2>
-                <p>Masters in Civil Engineering</p>
-                <a href="#" class="btn btn-outline-primary">Check questions</a>
-            </div>
-            <div class="col-lg-4 homepage-course-container my-5">
-                <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+            <div class="col-md-5 homepage-feature-image">
+                <svg class="img-fluid mx-auto" width="500" height="300" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
                     <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+                    <rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
                 </svg>
-                <h2 class="mt-4">MIEEC</h2>
-                <p>Master in Electrical and Computers Engineering</p>
-                <a href="#" class="btn btn-outline-primary">Check questions</a>
             </div>
+
+
+        </div>
+        <hr>
+        <!-- Courses -->
+        <div class="pt-3 homepage-course">
+            <h2 class="text-center mt-5">Courses</h2>
+            <div class="row">
+                <div class="col-lg-4 homepage-course-container my-5">
+                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>MIEIC</title>
+                        <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+                    </svg>
+                    <h2 class="mt-4">MIEIC</h2>
+                    <p>Matesters in Informatics and Computation Engineering</p>
+                    <a href="#" class="btn btn-outline-primary">Check questions</a>
+                </div>
+                <div class="col-lg-4 homepage-course-container my-5">
+                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>MIEEC</title>
+                        <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+                    </svg>
+                    <h2 class="mt-4">MIEC</h2>
+                    <p>Masters in Civil Engineering</p>
+                    <a href="#" class="btn btn-outline-primary">Check questions</a>
+                </div>
+                <div class="col-lg-4 homepage-course-container my-5">
+                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+                    </svg>
+                    <h2 class="mt-4">MIEEC</h2>
+                    <p>Master in Electrical and Computers Engineering</p>
+                    <a href="#" class="btn btn-outline-primary">Check questions</a>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+        <!-- Last search button in page -->
+        <div class="homepage-lastsearch my-5 d-flex mx-auto flex-column text-center">
+            <h1 class="display-5 mb-4 fw-normal">Looking for questions to answer?</h1>
+            <p class="lead mb-5">Don't forget that other students are also needing your help!
+                Learning-by-teaching is one of our lemmas. By explaining and sharing your knowledge you're able to increase
+                your comprehension in a topic. Show what you have learned and don't forget to have fun!
+            </p>
+            <p><a href="#" class="btn btn-outline-primary mb-5">Search</a></p>
         </div>
     </div>
 
-    <hr>
-    <!-- Last search button in page -->
-    <div class="homepage-lastsearch my-5 d-flex mx-auto flex-column text-center">
-        <h1 class="display-5 mb-4 fw-normal">Looking for questions to answer?</h1>
-        <p class="lead mb-5">Don't forget that other students are also needing your help!
-            Learning-by-teaching is one of our lemmas. By explaining and sharing your knowledge you're able to increase
-            your comprehension in a topic. Show what you have learned and don't forget to have fun!
-        </p>
-        <p><a href="#" class="btn btn-outline-primary mb-5">Search</a></p>
-    </div>
 </div>
