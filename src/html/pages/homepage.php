01a51da48f4431b61fea104b578139f1e21cc23f
Juliane Marubayashi <up201800175@fe.up.pt>
2021-03-03 13:33:50 +0000

    Missing slide for carousel

--- src/html/pages/homepage.php
+++ src/html/pages/homepage.php
@@ -1,27 +1,76 @@
 <?php
 
 include_once(dirname(__DIR__) . "\common\header.php");
-?> 
 
-<div class="position-relative text-center homepage-container-hero text-light">
-        <div class="banner-text col-md-5 mx-auto">
-            <h1 class="display-3"> BrainShare </h1>
-            <p class="lead fw-normal mb-3"> The FEUP website for questions and answers. We connect students to solutions to improve your academic grow. </p>
-            <div class="d-flex justify-content-center">
-                <a href="#" class="btn btn-primary mx-3">Search</a>
-                <a href="#" class="btn btn-primary mx-3">Add Question</a>
+$question =
+    '<article class="profile-question container my-4 border shadow rounded">
+        <!-- Question Title -->
+        <div class="row">
+            <div class="col-lg-2">
+                <h4>0 answers</h4>
+            </div>
+
+            <div class="d-flex align-items-center col-lg">
+                <h2 class="card-title flex-grow-1"><a href="#">Should I learn MIPS?</a></h2> <!-- Question Title -->
+
+                <span class="category course badge rounded-pill bg-secondary">
+                    <i class="fas fa-graduation-cap"></i>
+                    MIEIC
+                </span>
             </div>
         </div>
 
+        <div class="row">
+            <section class="col-lg-2">
+                <span class="category-profile-question category tag badge bg-secondary"> 
+                    <i class="fas fa-hashtag"></i>
+                    MIPS
+                </span>
+                <span class="category-profile-question category tag badge bg-secondary">
+                    <i class="fas fa-hashtag"></i>
+                    COMP
+                </span>
+            </section>
+
+            <!-- Question Text -->
+            <div class="question-content col-lg">
+                <p>Is the MIPS programming language that beneficial to know? Im a CS student and am taking a assembly class which focuses on MIPS. Im very comfortable writing in high level languages, but MIPS has me a little bit down. Is MIPS something that I should really focus on and try to completely grasp it? Will it help me in the future?</p>
+            </div>
+        </div>
+
+        <!-- Question Date -->
+        <section class="date-user-text">
+            <p>
+                <img class="rounded-circle date-user-image" src="images/profile.png" alt="Profile Image">
+                <span>Joaquina Almeida</span>
+                <span >15:02 - 10/05/2009</span>
+            </p>
+        </section>
+        </article>';
+
+
+?>
+
+<div class="position-relative text-center homepage-container-hero text-light">
+    <div class="banner-text col-md-5 mx-auto">
+        <h1 class="display-3"> BrainShare </h1>
+        <p class="lead fw-normal mb-3"> The FEUP website for questions and answers. We connect students to solutions to improve your academic grow. </p>
+        <div class="d-flex justify-content-center">
+            <a href="#" class="btn btn-primary mx-3">Search</a>
+            <a href="#" class="btn btn-primary mx-3">Add Question</a>
+        </div>
     </div>
+
+</div>
 
 <div class="page-margin">
 
     <div class="mx-3">
+
         <!-- Featured text  -->
         <div class="row py-5 homepage-feature">
             <div class="offset-md-1 col-md-6 homepage-feature-text">
-                <h2 class="mb-4 ">What is BrainShare?  <span class="text-muted ">The way to learn.</span> </h2>
+                <h2 class="mb-4 ">What is BrainShare? <span class="text-muted ">The way to learn.</span> </h2>
                 <p class="mb-5">BrainShare is a Q&A forum that allows FEUP students to answer their own doubts about about their courses!
                     All our features are completely free, since our goal is to be the center of information in our college.</p>
                 <a href="#" class="btn btn-secondary mb-4">About Us</a>
@@ -35,10 +84,12 @@ include_once(dirname(__DIR__) . "\common\header.php");
 
 
         </div>
+
         <hr>
+
         <!-- Courses -->
-        <div class="pt-3 homepage-course">
-            <h2 class="text-center mt-5">Courses</h2>
+        <section class="pt-3 homepage-course"> 
+            <h2 class="mb-4 fw-normal text-center">Courses</h2>
             <div class="row">
                 <div class="col-lg-4 homepage-course-container my-5">
                     <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
@@ -68,18 +119,53 @@ include_once(dirname(__DIR__) . "\common\header.php");
                     <a href="#" class="btn btn-outline-primary">Check questions</a>
                 </div>
             </div>
+        </section>
+
+        <hr>
+
+        <!-- Carousel  -->
+        <div class="carousel slide my-4" data-ride="carousel" id="featuretted-questions"> 
+            <h2 class="mb-4 fw-normal text-center">Featuretted questions</h2>
+            <!-- Featuretted questions -->
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <?= $question ?>
+                </div>
+                <div class="carousel-item">
+                    <span>Shit</span>
+                </div>
+                <div class="carousel-item">
+                    <?= $question ?>
+                </div>
+                <div class="carousel-item">
+                    <?= $question ?>
+                </div>
+            </div>
+
+            <!-- Nav buttons -->
+
+            <a class="carousel-control-prev" role="button" data-slide="prev" href="#featuretted-questions">
+                <span class="display-4 text-dark"> < </span>
+                <span class="sr-only">Next</span>
+            </a>
+            <a class="carousel-control-next" role="button" data-slide="next" href="#featuretted-questions">
+                <span class="display-4 text-dark"> > </span>
+                <span class="sr-only">Previous</span>
+            </a>
+
         </div>
 
         <hr>
+
         <!-- Last search button in page -->
-        <div class="homepage-lastsearch my-5 d-flex mx-auto flex-column text-center">
-            <h1 class="display-5 mb-4 fw-normal">Looking for questions to answer?</h1>
+        <section class="homepage-lastsearch my-5 d-flex mx-auto flex-column text-center">
+            <h2 class="mb-4 fw-normal">Looking for questions to answer?</h2>
             <p class="lead mb-5">Don't forget that other students are also needing your help!
                 Learning-by-teaching is one of our lemmas. By explaining and sharing your knowledge you're able to increase
                 your comprehension in a topic. Show what you have learned and don't forget to have fun!
             </p>
             <p><a href="#" class="btn btn-outline-primary mb-5">Search</a></p>
-        </div>
+        </section>
     </div>
 
 </div>
