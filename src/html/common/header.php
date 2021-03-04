35dfba3cdd87ab4ac1391538f707e33a938a2677
Diana Freitas <up201806230@fe.up.pt>
2021-03-04 08:26:22 +0000

    Head scripts and stylesheets organized

--- src/html/common/header.php
+++ src/html/common/header.php
@@ -5,22 +5,29 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Bootstrap Css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+    
+    <!-- Our Css -->
     <link href="css/styles.css" rel="stylesheet">
 
+    <!-- Bootstrap script -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous" defer></script>
-    <script src="https://kit.fontawesome.com/cf05cece41.js" crossorigin="anonymous"></script>
-    <script src="js/pagination.js" defer></script>
     
+    <!-- Font Awesom -->
+    <script src="https://kit.fontawesome.com/cf05cece41.js" crossorigin="anonymous"></script>
+    
+    <!-- Pagination -->
+    <script src="js/pagination.js" defer></script>
 
     <!-- Rich Text -->
     <link rel="stylesheet" href="http://lab.lepture.com/editor/editor.css" />
     <script type="text/javascript" src="http://lab.lepture.com/editor/editor.js" defer></script>
     <script type="text/javascript" src="http://lab.lepture.com/editor/marked.js" defer></script>
-
+    
+    <!-- Our Rich Text script -->
     <script src="js/editor.js" defer></script>
-    <!-- Rich Text -->
     
     <title>BrainShare</title> 
 
@@ -85,7 +92,7 @@
             <?php // Registered Users options
               if(/*isset($_SESSION['username']) && ($_SESSION['username'] !== '')*/true){ ?>
                 <a class="nav-link profile-button registration-button">
-                    <img src="images/profile.png" alt="">
+                    <img src="images/profile.png" alt="profile picture" class="rounded-circle">
                     Joaquina Almeida<!-- Name goes here -->
                 </a>
                 <div class="registration-button icon-hover notifications">
