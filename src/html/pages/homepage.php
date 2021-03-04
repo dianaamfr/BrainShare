f27b170aecb0dc549015386ea4829069930d4249
Diana Freitas <up201806230@fe.up.pt>
2021-03-04 15:58:30 +0000

    Added courses icons and images; Css

--- src/html/pages/homepage.php
+++ src/html/pages/homepage.php
@@ -13,7 +13,7 @@ $question1 =
             <div class="d-flex align-items-center col-lg">
                 <h2 class="card-title flex-grow-1"><a href="#">Should I learn MIPS?</a></h2> <!-- Question Title -->
 
-                <span class="category course badge rounded-pill bg-secondary">
+                <span class="category course badge rounded-pill bg-primarya">
                     <i class="fas fa-graduation-cap"></i>
                     MIEIC
                 </span>
@@ -142,6 +142,7 @@ $question3 =
 
 ?>
 
+<!-- Banner -->
 <div class="position-relative text-center homepage-container-hero text-light">
     <div class="banner-text col-md-5 mx-auto">
         <h1 class="display-3"> BrainShare </h1>
@@ -160,59 +161,40 @@ $question3 =
 
         <!-- Featured text  -->
         <div class="row py-5 homepage-feature">
-            <div class="offset-md-1 col-md-6 homepage-feature-text">
+            <div class="col-md-6 homepage-feature-text">
                 <h2 class="mb-4 ">What is BrainShare? <span class="text-muted ">The way to learn.</span> </h2>
                 <p class="mb-5">BrainShare is a Q&A forum that allows FEUP students to answer their own doubts about about their courses!
                     All our features are completely free, since our goal is to be the center of information in our college.</p>
-                <a href="#" class="btn btn-secondary mb-4">About Us</a>
+                <a href="#" class="btn btn-outline-primary mb-4">About Us</a>
             </div>
-            <div class="col-md-5 homepage-feature-image">
-                <svg class="img-fluid mx-auto" width="500" height="300" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
-                </svg>
+            <div class="col-md-5 homepage-feature-image1">
             </div>
-
-
         </div>
 
-        <hr>
-
         <!-- Courses -->
-        <section class="pt-3 homepage-course"> 
-            <h2 class="mb-4 fw-normal text-center">Courses</h2>
+        <section class="homepage-course"> 
+            <h2 class="mb-4 text-center">Courses</h2>
             <div class="row">
-                <div class="col-lg-4 homepage-course-container my-5">
-                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
-                        <title>MIEIC</title>
-                        <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
-                    </svg>
-                    <h2 class="mt-4">MIEIC</h2>
+                <div class="col-lg-4 homepage-course-container my-5 d-flex flex-column align-items-center">
+                    <div class="course-icon" id="course-icon1"></div>
+                    <h3 class="mt-4">MIEIC</h3>
                     <p>Matesters in Informatics and Computation Engineering</p>
                     <a href="#" class="btn btn-outline-primary">Check questions</a>
                 </div>
-                <div class="col-lg-4 homepage-course-container my-5">
-                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
-                        <title>MIEEC</title>
-                        <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
-                    </svg>
-                    <h2 class="mt-4">MIEC</h2>
+                <div class="col-lg-4 homepage-course-container my-5 d-flex flex-column align-items-center">
+                    <div class="course-icon" id="course-icon2"></div>
+                    <h3 class="mt-4">MIEC</h3>
                     <p>Masters in Civil Engineering</p>
                     <a href="#" class="btn btn-outline-primary">Check questions</a>
                 </div>
-                <div class="col-lg-4 homepage-course-container my-5">
-                    <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
-                        <title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
-                    </svg>
-                    <h2 class="mt-4">MIEEC</h2>
+                <div class="col-lg-4 homepage-course-container my-5 d-flex flex-column align-items-center">
+                    <div class="course-icon" id="course-icon3"></div>
+                    <h3 class="mt-4">MIEEC</h3>
                     <p>Master in Electrical and Computers Engineering</p>
                     <a href="#" class="btn btn-outline-primary">Check questions</a>
                 </div>
             </div>
         </section>
-
-        <hr>
 
             <!-- Carousel  
             <div class="carousel slide my-4" data-ride="carousel" id="featuretted-questions"> 
@@ -251,17 +233,6 @@ $question3 =
 
         </div>
 
-        <hr>
-
-        <!-- Last search button in page -->
-        <section class="homepage-lastsearch my-5 d-flex mx-auto flex-column text-center">
-            <h2 class="mb-4 fw-normal">Looking for questions to answer?</h2>
-            <p class="lead mb-5">Don't forget that other students are also needing your help!
-                Learning-by-teaching is one of our lemmas. By explaining and sharing your knowledge you're able to increase
-                your comprehension in a topic. Show what you have learned and don't forget to have fun!
-            </p>
-            <p><a href="#" class="btn btn-outline-primary mb-5">Search</a></p>
-        </section>
     </div>
 
 </div>
