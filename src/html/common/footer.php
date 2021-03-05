aeb5faa94300cfa841927785106f3303148f7f46
Diana Freitas <up201806230@fe.up.pt>
2021-03-05 16:19:36 +0000

    Old Footer

--- src/html/common/footer.php
+++ src/html/common/footer.php
@@ -1,4 +1,37 @@
 </main>
-
+    <footer class="py-5">
+        <div class="row container m-auto">
+            <div class="col-6 col-md">
+                <h5>Who we are</h5>
+                <ul class="list-unstyled text-small">
+                    <li><a class="link-secondary" href="#">Home</a></li>
+                    <li><a class="link-secondary" href="#">About us</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h5>Authentication</h5>
+                <ul class="list-unstyled text-small">
+                    <li><a class="link-secondary" href="#">Login</a></li>
+                    <li><a class="link-secondary" href="#">Register</a></li>
+                    <li><a class="link-secondary" href="#">Profile</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h5>Features</h5>
+                <ul class="list-unstyled text-small">
+                    <li><a class="link-secondary" href="#">Search Questions</a></li>
+                    <li><a class="link-secondary" href="#">Add Question</a></li>
+                    <li><a class="link-secondary" href="#">Search Users</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h5>Management</h5>
+                <ul class="list-unstyled text-small">
+                    <li><a class="link-secondary" href="#">Manage Categories</a></li>
+                    <li><a class="link-secondary" href="#">Reports</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
