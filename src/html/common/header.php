6db4ebdb9d1f6bf306a914646e176ba554ae9518
Juliane Marubayashi <up201800175@fe.up.pt>
2021-03-05 13:56:53 +0000

    Added script in header and fixed error in pagination.js

--- src/html/common/header.php
+++ src/html/common/header.php
@@ -29,6 +29,9 @@
     <!-- Our Rich Text script -->
     <script src="js/editor.js" defer></script>
     
+    <!-- Library to translate MD to html  -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/showdown/1.9.1/showdown.min.js" integrity="sha512-L03kznCrNOfVxOUovR6ESfCz9Gfny7gihUX/huVbQB9zjODtYpxaVtIaAkpetoiyV2eqWbvxMH9fiSv5enX7bw==" crossorigin="anonymous"></script> 
+    
     <title>BrainShare</title> 
 
   </head>
---
+++ src/js/parseMD.js
@@ -0,0 +1,13 @@
+
+
+const converter = new showdown.Converter(); 
+
+let text = document.getElementsByClassName('question-content')[0];  
+console.log(text);  
+
+/*
+var converter = new showdown.Converter(),
+    text      = '# hello, markdown!',
+    html      = converter.makeHtml(text); 
+
+*/ 
