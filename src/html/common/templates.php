a3b2aba15fd9545cf9eb22af939c6784d64f9220
up201806490 <up201806490@fe.up.pt>
2021-03-06 17:46:44 +0000

    Added href to questions

--- src/html/common/templates.php
+++ src/html/common/templates.php
@@ -1,15 +1,82 @@
 <?php
     function questionPreview(){ ?>
-        <article class="question-preview card flex-row align-items-center">
-            <div class="counts">
-            <div>2</div>
-            <div>answers</div>
-            </div>
-            <div class="counts">
-            <div>1</div>
-            <div>votes</div>
-            </div>
-            <div class="card-body">
+        <a href="question.php" class="question-href">
+            <article class="question-preview card flex-row align-items-center">
+                <div class="counts">
+                <div>2</div>
+                <div>answers</div>
+                </div>
+                <div class="counts">
+                <div>1</div>
+                <div>votes</div>
+                </div>
+                <div class="card-body">
+                    <header class="card-header">
+                        <div class="question-header d-flex align-items-center">
+
+                            <!-- Mobile Question details -->
+                            <div class="d-none question-details d-flex mb-3">
+                                <!-- Course -->
+                                <span class="category course badge rounded-pill bg-secondary">
+                                    <i class="fas fa-graduation-cap"></i>
+                                    MIEIC
+                                </span>
+                            </div>
+
+                            <!-- Question Title -->
+                            <h4 class="card-title flex-grow-1">Should I learn MIPS? Or is it a waist of time nowadays? </h4> <!-- Question Title -->
+
+                            <!-- Desktop Question details -->
+                            <div class="question-details d-flex">
+                                <!-- Course -->
+                                <span class="category course badge rounded-pill bg-secondary">
+                                    <i class="fas fa-graduation-cap"></i>
+                                    MIEIC
+                                </span>
+
+                            </div>
+                        </div>
+                    </header>
+
+                    <!-- Tags and User -->
+                    <footer class="card-footer d-flex align-items-center flex-wrap">
+                        <div class="flex-grow-1 mb-1">
+                            <span class="category tag badge bg-secondary"> 
+                                <i class="fas fa-hashtag"></i>
+                                MIPS
+                            </span>
+                            <span class="category tag badge bg-secondary">
+                                <i class="fas fa-hashtag"></i>
+                                COMP
+                            </span>
+                        </div>
+                        <div class="question-author d-inline-flex align-items-center">
+                            <img class="rounded-circle" src="images/profile.png" alt=""> <!-- Small Profile Image -->
+                            <div class="d-flex flex-wrap">
+                                <span>Joaquina Almeida</span> <!-- Name -->
+                                <span >15:02 - 10/05/2009</span> <!-- Date -->
+                            </div>
+                        </div>
+                    </footer>
+                </div>
+                <div class="counts-mobile">
+                    <div>2 answers</div>
+                    <div>1 votes</div>
+                </div>
+            </article>
+        </a>
+
+        <a href="question.php" class="question-href">
+            <article class="question-preview card flex-row align-items-center">
+                <div class="counts">
+                    <div>5</div>
+                    <div>answers</div>
+                </div>
+                <div class="counts">
+                    <div>2</div>
+                    <div>votes</div>
+                </div>
+                <div class="card-body">
                 <header class="card-header">
                     <div class="question-header d-flex align-items-center">
 
@@ -23,7 +90,7 @@
                         </div>
 
                         <!-- Question Title -->
-                        <h4 class="card-title flex-grow-1">Should I learn MIPS? Or is it a waist of time nowadays? </h4> <!-- Question Title -->
+                        <h4 class="card-title flex-grow-1">I can't solve this integral!</h4> <!-- Question Title -->
 
                         <!-- Desktop Question details -->
                         <div class="question-details d-flex">
@@ -42,11 +109,7 @@
                     <div class="flex-grow-1 mb-1">
                         <span class="category tag badge bg-secondary"> 
                             <i class="fas fa-hashtag"></i>
-                            MIPS
-                        </span>
-                        <span class="category tag badge bg-secondary">
-                            <i class="fas fa-hashtag"></i>
-                            COMP
+                            Amat
                         </span>
                     </div>
                     <div class="question-author d-inline-flex align-items-center">
@@ -57,74 +120,14 @@
                         </div>
                     </div>
                 </footer>
-            </div>
-            <div class="counts-mobile">
-                <div>2 answers</div>
-                <div>1 votes</div>
-            </div>
-        </article>
-
-
-        <article class="question-preview card flex-row align-items-center">
-            <div class="counts">
-                <div>5</div>
-                <div>answers</div>
-            </div>
-            <div class="counts">
-                <div>2</div>
-                <div>votes</div>
-            </div>
-            <div class="card-body">
-            <header class="card-header">
-                <div class="question-header d-flex align-items-center">
-
-                    <!-- Mobile Question details -->
-                    <div class="d-none question-details d-flex mb-3">
-                        <!-- Course -->
-                        <span class="category course badge rounded-pill bg-secondary">
-                            <i class="fas fa-graduation-cap"></i>
-                            MIEIC
-                        </span>
-                    </div>
-
-                    <!-- Question Title -->
-                    <h4 class="card-title flex-grow-1">I can't solve this integral!</h4> <!-- Question Title -->
-
-                    <!-- Desktop Question details -->
-                    <div class="question-details d-flex">
-                        <!-- Course -->
-                        <span class="category course badge rounded-pill bg-secondary">
-                            <i class="fas fa-graduation-cap"></i>
-                            MIEIC
-                        </span>
-
-                    </div>
+                
+                <div class="counts-mobile">
+                    <div>2 answers</div>
+                    <div>2 votes</div>
                 </div>
-            </header>
-
-            <!-- Tags and User -->
-            <footer class="card-footer d-flex align-items-center flex-wrap">
-                <div class="flex-grow-1 mb-1">
-                    <span class="category tag badge bg-secondary"> 
-                        <i class="fas fa-hashtag"></i>
-                        Amat
-                    </span>
                 </div>
-                <div class="question-author d-inline-flex align-items-center">
-                    <img class="rounded-circle" src="images/profile.png" alt=""> <!-- Small Profile Image -->
-                    <div class="d-flex flex-wrap">
-                        <span>Joaquina Almeida</span> <!-- Name -->
-                        <span >15:02 - 10/05/2009</span> <!-- Date -->
-                    </div>
-                </div>
-            </footer>
-            
-            <div class="counts-mobile">
-                <div>2 answers</div>
-                <div>2 votes</div>
-            </div>
-            </div>
-        </article>
+            </article>
+        </a>
 <?php } ?>
 
 <?php
