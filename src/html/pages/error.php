36b85bd817bbdd2d07a4d148127c0d0d607585b0
Juliane Marubayashi <up201800175@fe.up.pt>
2021-03-09 00:57:24 +0000

    Added page error/404

---
+++ src/html/pages/error.php
@@ -0,0 +1,9 @@
+<div class="my-bg-main-blue mb-4 error text-center">
+    <h1>404!</h1>
+    <p class="lead"> Nothing to find here! Try to find your questions in another page.</p>
+</div>
+
+<div class="d-flex justify-content-center my-4">
+    <a href="search.php" class="btn btn-primary mx-3">Search</a>
+    <a href="add-question.php" class="btn btn-primary mx-3">Add Question</a>
+</div>
