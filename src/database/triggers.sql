8d376addf2b9d674044b008c2ea6d7bfdca57b5d
Diogo Samuel Fernandes <up201806250@fe.up.pt>
2021-03-30 18:29:56 +0000

    Triggers for vote

--- src/database/triggers.sql
+++ src/database/triggers.sql
@@ -41,3 +41,84 @@ CREATE TRIGGER comment_notification
 	
 INSERT INTO comment(id, answer_id, comment_owner_id, content, "date") VALUES (6, 8, 5, 'Está na parte dos recursos de lbaw!', '2021-03-30');
 SELECT * FROM "notification";
+
+/* Um user não pode dar upvote na própria questão */
+DROP FUNCTION IF EXISTS process_vote() CASCADE;
+DROP TRIGGER IF EXISTS vote_trigger ON vote;
+
+CREATE FUNCTION process_vote() RETURNS TRIGGER AS $$
+BEGIN
+  IF 
+  	NEW.answer_id IS NOT NULL AND
+  	EXISTS (SELECT * FROM answer WHERE answer.id = NEW.answer_id AND 
+		NEW.user_id = answer.answer_owner_id)
+  THEN
+  	RAISE EXCEPTION 'Cant vote own answer';
+   ELSIF 
+  	NEW.question_id IS NOT NULL AND
+  	EXISTS (SELECT * FROM question WHERE question.id = NEW.question_id AND 
+		NEW.user_id = question.question_owner_id)
+	 
+	 NOT EXISTS
+  THEN
+  	RAISE EXCEPTION 'Cant vote own question'; 
+  END IF;
+  RETURN NEW;
+END
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER vote_trigger
+    BEFORE INSERT ON vote
+    FOR EACH ROW
+    EXECUTE PROCEDURE process_vote();
+
+/*
+INSERT INTO "vote" (id,user_id,answer_id,value_vote) VALUES (DEFAULT,2,4,'1');
+INSERT INTO "vote" (id,user_id,answer_id,value_vote) VALUES (DEFAULT, 45, 5,'1');
+*/
+
+/*
+INSERT INTO "vote" (id,user_id,question_id,value_vote) VALUES (DEFAULT,96,4,'-1');
+INSERT INTO "vote" (id,user_id,question_id,value_vote) VALUES (DEFAULT,1,1,'-1');
+*/
+	
+/* When user votes a question we already voted with the same "score", the upvote disappear. 
+If the score is different, the score is updated */
+DROP FUNCTION IF EXISTS update_vote() CASCADE;
+DROP TRIGGER IF EXISTS update_vote_trigger ON vote;
+
+CREATE FUNCTION update_vote() RETURNS TRIGGER AS $$
+DECLARE old_vote_id INTEGER;
+BEGIN
+  SELECT INTO old_vote_id vote.id FROM vote WHERE new.user_id = vote.user_id AND 
+             (new.answer_id = vote.answer_id OR new.question_id = vote.question_id);
+  IF old_vote_id IS NOT NULL
+  THEN
+      IF EXISTS (SELECT * FROM vote WHERE vote.id = old_vote_id AND 
+                 vote.value_vote = new.value_vote)
+      THEN
+	  	  DELETE FROM "vote"
+    		WHERE "vote".id = old_vote_id;
+		  RETURN NULL;
+      ELSE
+	  	  UPDATE vote SET
+      		value_vote = -1 * value_vote
+      		WHERE id = old_vote_id;
+          RETURN NULL;
+    END IF;
+  END IF;
+  RETURN NEW;
+END
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER update_vote_trigger
+    BEFORE INSERT ON vote
+    FOR EACH ROW
+    EXECUTE PROCEDURE update_vote();
+
+
+-- INSERT INTO "vote" (id,user_id,question_id,value_vote) VALUES (90, 32, 6 ,'-1');
+INSERT INTO "vote" (id,user_id,question_id,value_vote) VALUES (90, 32, 6 ,'-1');
+SELECT * FROM "vote" WHERE "vote".id = 90;
