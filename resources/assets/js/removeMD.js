3dc7d2e8d9b7af491b86ff461e1e2f0174f11930
Diogo Samuel Fernandes <up201806250@fe.up.pt>
2021-04-20 18:50:09 +0000

    Start A3 migration

---
+++ resources/assets/js/removeMD.js
@@ -0,0 +1,26 @@
+// See how this works: https://stackoverflow.com/questions/822452/strip-html-from-text-javascript 
+
+/**
+ * To remove the md it's necessary to have the item needs to have the classes md-content and md-remove.  
+ * The content must be envolved in a div. 
+ * Example: 
+ * <div class="md-content md-remove"> </div> 
+ */
+
+try{
+let remove_md_list = document.querySelectorAll(".md-content.md-remove");  
+
+remove_md_list.forEach((element) => {
+    element.innerHTML = stripHtml(element.innerHTML); 
+});  
+
+} catch(e){
+    console.warn("No md to remove");
+}
+
+
+function stripHtml(html) {
+   let tmp = document.createElement("DIV");
+   tmp.innerHTML = html; 
+   return tmp.textContent || tmp.innerText || "";
+} 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,11 +8,12 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name','Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/milligram.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/responsive.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    
     <script type="text/javascript">
         // Fix for Firefox autofocus CSS bug
         // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
@@ -22,12 +23,17 @@
   </head>
   <body>
     <main>
-      <header>
-        <h1><a href="{{ url('/cards') }}">Thingy!</a></h1>
+      <header class="fixed-top m-auto">
+
+
+
+        <h1><a href="{{ url('/cards') }}">BrainShare</a></h1>
         @if (Auth::check())
         <a class="button" href="{{ url('/logout') }}"> Logout </a> <span>{{ Auth::user()->name }}</span>
         @endif
+
       </header>
+
       <section id="content">
         @yield('content')
       </section>
