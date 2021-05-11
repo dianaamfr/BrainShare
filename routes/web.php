9e1d37c18be371c0f11ee5b9a2139404754b9128
Diana Freitas <up201806230@fe.up.pt>
2021-05-11 14:50:43 +0100

    Logout back again

--- routes/web.php
+++ routes/web.php
@@ -52,7 +52,7 @@ Route::delete('/api/question/{id-q}/comment/{id-c}','CommentController@deleteCom
 // Module M01: Authentication
 Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
-Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('register', 'Auth\RegisterController@register');
 
