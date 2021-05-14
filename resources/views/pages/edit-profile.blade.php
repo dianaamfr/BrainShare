e51630e2fa1c2c4da8faa1fab5e9e6d551261252
Juliane Marubayashi <up201800175@fe.up.pt>
2021-05-14 19:16:15 +0100

    Layout to change password and delete user

--- resources/views/pages/edit-profile.blade.php
+++ resources/views/pages/edit-profile.blade.php
@@ -37,8 +37,6 @@
                 </div>
 
                 <div class="form-edit-profile">
-
-
                     <div class="two row">
                         <section class="profile-info col-md mb-4">
                             <h3>Personal</h3>
@@ -92,20 +90,69 @@
                                     </span>
                                 @endif
                             </div>
-                        </section>
 
+                            <h3 class="mt-4">Change Password</h3>
+
+                            <!-- Password -->
+                            <div class="mb-2">
+                                <label class="form-label">Password</label>
+                                <div class="input-group mb-0">
+                                    <div class="input-group-prepend" title="Password">
+                                        <span class="input-group-text"><i class="fas fa-key"></i></span>
+                                    </div>
+                                    <input type="password" class="form-control" name="password" required>
+                                </div>
+                                @if ($errors->has('email'))
+                                    <span class="error">
+                        {{ $errors->first('email') }}
+                    </span>
+                                @endif
+                            </div>
+
+                            <div class="mb-2">
+                                <label class="form-label">Password Confirmation</label>
+                                <div class="input-group mb-0">
+                                    <div class="input-group-prepend" title="Password-Confirmation">
+                                        <span class="input-group-text"><i class="fas fa-key"></i></span>
+                                    </div>
+                                    <input type="password" class="form-control" name="password-confirmation" required>
+                                </div>
+                                @if ($errors->has('email'))
+                                    <span class="error">
+                                {{ $errors->first('email') }}
+                            </span>
+                                @endif
+                            </div>
+
+                            <div class="mb-2">
+                                <label class="form-label">New Password</label>
+                                <div class="input-group mb-0">
+                                    <div class="input-group-prepend" title="Password-Confirmation">
+                                        <span class="input-group-text"><i class="fas fa-key"></i></span>
+                                    </div>
+                                    <input type="password" class="form-control" name="password-confirmation" required>
+                                </div>
+                                @if ($errors->has('email'))
+                                    <span class="error">
+                                {{ $errors->first('email') }}
+                            </span>
+                                @endif
+                            </div>
+
+
+                        </section>
                         <!-- About me -->
-                        <section class="profile-about-me col-md mb-4">
+                        <section class="profile-about-me col-md mb-4 h-100">
                             <h3>About Me</h3>
                             <div class="mb-3">
-                                <textarea class="form-control about-me"
-                                          name="description">{{$user->description}}</textarea>
+                            <textarea class="form-control about-me"
+                                      name="description">{{$user->description}}</textarea>
                                 <div id="questionBodyHelp" class="form-text">Describe all the details about you!</div>
                             </div>
                             @if ($errors->has('description'))
                                 <span class="error">
-                                        {{ $errors->first('description') }}
-                                </span>
+                                    {{ $errors->first('description') }}
+                            </span>
                             @endif
                         </section>
 
@@ -142,14 +189,64 @@
 
                     <div class="d-md-flex">
                         <button type="submit" class="btn btn-primary mt-3">Save Changes</button>
-                        <button type="button" class="btn btn-outline-primary mx-2 mt-3" onclick="window.location='{{URL::route('show-profile', $user->id)}}'">Cancel</button>
-                        <button type="button" class="btn btn-outline-danger mt-3 ms-md-auto">Delete Account</button>
+                        <button type="button" class="btn btn-outline-primary mx-2 mt-3"
+                                onclick="window.location='{{URL::route('show-profile', $user->id)}}'">Cancel
+                        </button>
                     </div>
 
                 </div>
 
             </section>
         </form>
+
+        <section id="profile-main" class="card profile-info container-fluid mt-2">
+
+            <h3>Delete account</h3>
+            <div class="row">
+                <div class="col-lg-5 mt-3">
+                    <span>By deleting you account you profile will not be visible anymore.
+                    Your questions, answers and comments will still be visible and available at the
+                        website, however the author will be identified as anonymous.
+                    </span>
+                </div>
+
+                <div class="col-lg-7 mt-2">
+                    <!-- Password -->
+                    <div class="mb-2 w-70">
+                        <label class="form-label">Password*</label>
+                        <div class="input-group mb-0">
+                            <div class="input-group-prepend" title="Password">
+                                <span class="input-group-text"><i class="fas fa-key"></i></span>
+                            </div>
+                            <input type="password" class="form-control" name="password" required>
+                        </div>
+                        @if ($errors->has('email'))
+                            <span class="error">
+                        {{ $errors->first('email') }}
+                    </span>
+                        @endif
+                    </div>
+
+                    <div class="mb-2">
+                        <label class="form-label">Password Confirmation*</label>
+                        <div class="input-group mb-0">
+                            <div class="input-group-prepend" title="Password-Confirmation">
+                                <span class="input-group-text"><i class="fas fa-key"></i></span>
+                            </div>
+                            <input type="password" class="form-control" name="password-confirmation" required>
+                        </div>
+                        @if ($errors->has('email'))
+                            <span class="error">
+                                {{ $errors->first('email') }}
+                            </span>
+                        @endif
+                    </div>
+
+                    <button type="button" class="btn btn-outline-danger mt-3 ms-md-auto">Delete Account</button>
+                </div>
+            </div>
+        </section>
+
     </div>
 
     <script>const max_tags = 2</script>
