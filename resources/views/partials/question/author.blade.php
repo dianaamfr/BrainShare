8392f66f56c4cfb28f54748bc55f042d0b4c6910
Juliane Marubayashi <up201800175@fe.up.pt>
2021-05-22 14:37:35 +0100

    added link to the user

--- resources/views/partials/question/author.blade.php
+++ resources/views/partials/question/author.blade.php
@@ -1,7 +1,7 @@
 <div class="question-author d-inline-flex align-items-center">
     <img class="rounded-circle" src="{{asset('images/profile.png')}}" alt="Profile Image"> <!-- Small Profile Image -->
     <div class="d-flex flex-wrap">
-        <span>{{$question->owner ? $question->owner->username : 'UnknownUser'}}</span> <!-- Username -->
+        <a href="{{route('show-profile', ['id' =>$question->owner->id])}}"  ><span>{{$question->owner ? $question->owner->username : 'UnknownUser'}}</span></a> <!-- Username -->
         <span> {{ date('d-m-Y H:i', strtotime($question->date)) }} </span> <!-- Date -->
     </div>
 </div>
