36e66df1a5a46b4fbbbd3028a4abad2fe9912e48
Juliane Marubayashi <up201800175@fe.up.pt>
2021-05-25 15:06:27 +0100

    Author blade, anonymous fixed error

--- resources/views/partials/question/author.blade.php
+++ resources/views/partials/question/author.blade.php
@@ -1,7 +1,13 @@
 <div class="question-author d-inline-flex align-items-center">
     <img class="rounded-circle" src="{{asset('images/profile.png')}}" alt="Profile Image"> <!-- Small Profile Image -->
     <div class="d-flex flex-wrap">
-        <a href="{{route('show-profile', ['id' =>$question->owner->id])}}" class="text-reset"><span>{{$question->owner ? $question->owner->username : 'UnknownUser'}}</span></a> <!-- Username -->
+        @if ($question->owner)
+            <a href="{{route('show-profile', ['id' =>$question->owner->id])}}" class="text-reset">
+                <span>{{$question->owner->username}}</span>
+            </a> <!-- Username -->
+        @else
+            <span>Anonymous</span>
+        @endif
         <span> {{ date('d-m-Y H:i', strtotime($question->date)) }} </span> <!-- Date -->
     </div>
 </div>
