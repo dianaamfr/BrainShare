3807ae8db838c32b13f61145b27e03fa5427e648
Juliane Marubayashi <up201800175@fe.up.pt>
2021-05-25 14:54:03 +0100

    Update user policy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -10,26 +10,30 @@ use Illuminate\Support\Facades\Auth;
 class UserPolicy
 {
     use HandlesAuthorization;
-    
+
     public function show(User $user, User $profile){
 
       return Auth::check() && ($profile->ban === false || $user->isAdmin() || $user->isModerator());
     }
 
+
     public function showManageUsers(User $user){
       return Auth::check() && ($user->isAdmin() || $user->isModerator());
     }
 
-    public function updateState(User $user, User $updated){
-      if(!Auth::check()) return false;
-      
-      // Administrators can change the role of any user
-      if ($user->isAdmin()){
-        return true;
-      }
+    // Only the user can edit it's profile.
+    public function showEditUserProfile(User $user, User $editUser): bool
+    {
+        return $editUser->id == $user->id;
+    }
 
-      // Moderators can only change the role of Registered Users or of themselves
-      $updatedIsRegisteredUser =  !$updated->isAdmin() && !$updated->isModerator();
+    public function editUserProfile(User $user, User $editUser): bool
+    {
+        return $editUser->id == $user->id;
+    }
+
+    public function delete(User $user, User $deleted): bool
+    {
 
         // Administrators can delete any user
         if ($user->isAdmin()) {
@@ -43,19 +47,20 @@ class UserPolicy
 
         // The own user may delete his account
         return $user->id == $deleted->id;
+    } 
+
+   public function updateState(User $user, User $updated){
+      if(!Auth::check()) return false;
+      
+      // Administrators can change the role of any user
+      if ($user->isAdmin()){
+        return true;
+      }
+
+      // Moderators can only change the role of Registered Users or of themselves
+      $updatedIsRegisteredUser =  !$updated->isAdmin() && !$updated->isModerator();
+
+      return $updatedIsRegisteredUser && $user->isModerator();
     }
 
-    public function updateState(User $user, User $updated)
-    {
-
-        // Administrators can change the role of any user
-        if ($user->isAdmin()) {
-            return true;
-        }
-
-        // Moderators can only change the role of Registered Users or of themselves
-        $updatedIsRegisteredUser = !$updated->isAdmin() && !$updated->isModerator();
-
-        return $updatedIsRegisteredUser && $user->isModerator();
-    }
 }
