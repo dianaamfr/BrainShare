4fa3e60ef44b7f898848505ffd8d32838f6be5e9
Diana Freitas <up201806230@fe.up.pt>
2021-05-27 00:43:42 +0100

    Improved Tooltip messages

--- resources/views/partials/management/reports/report-actions.blade.php
+++ resources/views/partials/management/reports/report-actions.blade.php
@@ -5,9 +5,9 @@
         <!-- Handled User Reports -->
         @if($report->reported_id)
             @if ($report->reported->ban == true)
-                @include('partials.management.reports.report-button', ['icon' => 'fa-user-check', 'action' => 'revert', 'title' => 'Unban'])
+                @include('partials.management.reports.report-button', ['icon' => 'fa-user-check', 'action' => 'revert', 'title' => 'Unban User'])
             @else
-                @include('partials.management.reports.report-button', ['icon' => 'fa-user-times', 'action' => 'delete', 'title' => 'Ban'])
+                @include('partials.management.reports.report-button', ['icon' => 'fa-user-times', 'action' => 'delete', 'title' => 'Ban User'])
             @endif
         <!-- Handled Question, Answer, Comment Reports -->
         @elseif(($report->question_id && $report->question->deleted == true) || 
@@ -16,7 +16,7 @@
                 
                 @include('partials.management.reports.report-button', ['icon' => 'fa-trash-restore-alt', 'action' => 'revert', 'title' => 'Restore Content'])
         @else
-            @include('partials.management.reports.report-button', ['icon' => 'fa-trash-alt', 'action' => 'delete', 'title' => 'Delete Content'])
+            @include('partials.management.reports.report-button', ['icon' => 'fa-trash-alt', 'action' => 'delete', 'title' => $report->question_id ? 'Delete Question' : ($report->answer_id ? 'Delete Answer' : 'Delete Comment')])
         @endif
 
         @include('partials.management.reports.report-button', ['icon' => 'fa-undo', 'action' => 'undiscard', 'title' => 'Mark as Pending'])
@@ -24,15 +24,15 @@
     @else
         @if($report->reported_id)
             @if ($report->reported->ban != true)
-                @include('partials.management.reports.report-button', ['icon' => 'fa-user-times', 'action' => 'delete', 'title' => 'Ban'])
+                @include('partials.management.reports.report-button', ['icon' => 'fa-user-times', 'action' => 'delete', 'title' => 'Ban User'])
             @endif
         @elseif(($report->question_id && $report->question->deleted == false) || 
             ($report->answer_id && $report->answer->deleted == false) || 
             ($report->comment_id && $report->comment->deleted == false))
-            @include('partials.management.reports.report-button', ['icon' => 'fa-trash-alt', 'action' => 'delete', 'title' => 'Delete Content'])
+            @include('partials.management.reports.report-button', ['icon' => 'fa-trash-alt', 'action' => 'delete', 'title' => $report->question_id ? 'Delete Question' : ($report->answer_id ? 'Delete Answer' : 'Delete Comment')])
         @endif
 
-        @include('partials.management.reports.report-button', ['icon' => 'fa-file-excel', 'action' => 'discard', 'title' => 'Discard Report'])
+        @include('partials.management.reports.report-button', ['icon' => 'fa-times', 'action' => 'discard', 'title' => 'Discard Report'])
     @endif
 
 </div>
