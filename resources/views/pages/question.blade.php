96be2f029a0608916d09b1c9d95bb837183deb41
Juliane Marubayashi <up201800175@fe.up.pt>
2021-05-26 20:36:15 +0100

    Fixed search after merge

--- resources/views/pages/question.blade.php
+++ resources/views/pages/question.blade.php
@@ -2,84 +2,84 @@
 
 @section('content')
 
-    @section('scripts')
-        <script  src={{ asset('js/answer.js')}}  type="module"></script>
-        <script  src={{ asset('js/comment.js')}}  type="module"></script>
-        <script src={{ asset('js/report.js') }} type="module"></script>
-    @endsection
+@section('scripts')
+    <script src={{ asset('js/answer.js')}}  type="module"></script>
+    <script src={{ asset('js/comment.js')}}  type="module"></script>
+    <script src={{ asset('js/report.js') }} type="module"></script>
+@endsection
 
-    <div id="page-top" class="page-margin question-page">
-        <article class="question card">
-            <div class="card-body">
-                <header class="card-header">
-                    <div class="question-header d-flex align-items-center">
-                        <!-- Mobile Question details -->
-                        <div class="d-none question-details d-flex mb-3">
-                            <!-- Course -->
-                        @include('partials.question.courses')
+<div id="page-top" class="page-margin question-page">
+    <article class="question card">
+        <div class="card-body">
+            <header class="card-header">
+                <div class="question-header d-flex align-items-center">
+                    <!-- Mobile Question details -->
+                    <div class="d-none question-details d-flex mb-3">
+                        <!-- Course -->
+                    @include('partials.question.courses')
 
-                        <!-- Edit/Delete: only for Registred Users -->
-                            @include('partials.question.update', ['margin' => 'ms-auto'])
+                    <!-- Edit/Delete: only for Registred Users -->
+                        @include('partials.question.update', ['margin' => 'ms-auto'])
 
-                        </div>
-
-                        <!-- Question Title -->
-                        <h2 class="card-title flex-grow-1">{{$question->title}}</h2> <!-- Question Title -->
-
-                        <!-- Desktop Question details -->
-                        <div class="question-details d-flex">
-
-                        @include('partials.question.courses')
-
-                        <!-- Edit/Delete: only for Registred Users -->
-                            @include('partials.question.update', ['margin' => ''])
-                        </div>
                     </div>
 
-                    <!-- Question Owner details -->
-                    @include('partials.question.author')
+                    <!-- Question Title -->
+                    <h2 class="card-title flex-grow-1">{{$question->title}}</h2> <!-- Question Title -->
 
-                </header>
+                    <!-- Desktop Question details -->
+                    <div class="question-details d-flex">
 
-                <!-- Question Text -->
-                <div class="row align-items-center px-3">
-                    @include('partials.question.question-content')
+                    @include('partials.question.courses')
+
+                    <!-- Edit/Delete: only for Registred Users -->
+                        @include('partials.question.update', ['margin' => ''])
+                    </div>
                 </div>
 
+                <!-- Question Owner details -->
+                @include('partials.question.author')
 
-                <!-- Footer -->
-                <footer class="d-flex">
-
-                    <!-- Tags -->
-                @include('partials.question.tags')
-
-                <!-- Report Button -->
-                    @include('partials.common.report',['margin' => 'ms-auto', 'id'=>$question->id, 'type'=>'question'])
-
-                </footer>
-
-            </div>
-
-        </article>
-
-        <!-- Answer -->
-        <section class="answers">
-            <header class="d-flex align-items-center">
-                <h4 id="question-number-answers" class="d-inline-block">{{$question->number_answer}} answers</h4>
-                <a class="btn btn-primary ms-auto" href="#submit-answer">Add Answer</a>
             </header>
-            <div class="answer card" id="all-answers">
-                @include('partials.common.answer-list',['answer'=>$question->answers])
+
+            <!-- Question Text -->
+            <div class="row align-items-center px-3">
+                @include('partials.question.question-content')
             </div>
-        </section>
 
-        <!-- Submit Answer Form -->
-        <form id="submit-answer">
-            <input type="hidden" name="questionID" value="{{$question->id}}">
-            @include('partials.question.answer-form')
-        </form>
-    </div>
 
-    @include('partials.common.report-modal')
-    @include('partials.common.toast')
+            <!-- Footer -->
+            <footer class="d-flex">
+
+                <!-- Tags -->
+            @include('partials.question.tags')
+
+            <!-- Report Button -->
+                @include('partials.common.report',['margin' => 'ms-auto', 'id'=>$question->id, 'type'=>'question'])
+
+            </footer>
+
+        </div>
+
+    </article>
+
+    <!-- Answer -->
+    <section class="answers">
+        <header class="d-flex align-items-center">
+            <h4 id="question-number-answers" class="d-inline-block">{{$question->number_answer}} answers</h4>
+            <a class="btn btn-primary ms-auto" href="#submit-answer">Add Answer</a>
+        </header>
+        <div class="answer card" id="all-answers">
+            @include('partials.common.answer-list',['answer'=>$question->answers])
+        </div>
+    </section>
+
+    <!-- Submit Answer Form -->
+    <form id="submit-answer">
+        <input type="hidden" name="questionID" value="{{$question->id}}">
+        @include('partials.question.answer-form')
+    </form>
+</div>
+
+@include('partials.common.report-modal')
+@include('partials.common.toast')
 @endsection
--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -2,40 +2,36 @@
 
 @section('content')
 
+@section('scripts')
+    <script src={{ asset('js/toastReplies.js')}}  type="module"></script>
+@endsection
+
+@if(session("message") != null)
+    <script>let toastMessageReply = "{{session("message")}}"</script>
+    @include('partials.common.toast')
+    {{session()->forget("message")}}
+@endif
+
 <div class="page-margin" id="search-page">
-  <!-- Questions Results -->
-  <div class="mt-md-5">
-    <header>
-      <div id="search-header" class="d-flex">
-        <h2>Search</h2>
-        @section('scripts')
-            <script  src={{ asset('js/toastReplies.js')}}  type="module"></script>
-        @endsection
-
-        @if(session("message") != null)
-            <script>let toastMessageReply = "{{session("message")}}"</script>
-            @include('partials.common.toast')
-            {{session()->forget("message")}}
-        @endif
-
     <!-- Questions Results -->
-        <div class="mt-md-5">
-            <header>
-                <div id="search-header" class="d-flex">
-                    <h2>Search</h2>
+    <div class="mt-md-5">
+        <header>
+            <div id="search-header" class="d-flex">
+                <h2>Search</h2>
 
-                    <!-- Search Results Order -->
-                    @include('partials.search.order')
-                </div>
-        
-      <!-- Search Filters -->
-      @include('partials.search.filters')
+                <!-- Search Results Order -->
+                @include('partials.search.order')
+            </div>
 
-    </header>
-  </div>
-  <!-- Questions -->
-  <div class="mt-5 question-search-results">
-      @include('partials.search.search-questions', $questions)
-  </div>
+            <!-- Search Filters -->
+            @include('partials.search.filters')
+
+        </header>
+    </div>
+
+    <!-- Questions -->
+    <div class="mt-5 question-search-results">
+        @include('partials.search.search-questions', $questions)
+    </div>
 </div>
 @endsection
