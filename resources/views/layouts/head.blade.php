79c6e7424c3c95afd306b7010c32d6369f7e6cb4
Diana Freitas <up201806230@fe.up.pt>
2021-05-27 22:37:25 +0100

    Missing script

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -50,3 +50,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/showdown/1.9.1/showdown.min.js"
         integrity="sha512-L03kznCrNOfVxOUovR6ESfCz9Gfny7gihUX/huVbQB9zjODtYpxaVtIaAkpetoiyV2eqWbvxMH9fiSv5enX7bw=="
         crossorigin="anonymous"></script>
+
+@yield('scripts')
