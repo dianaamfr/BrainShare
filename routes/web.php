e6bc65e2816fe7f4f3f51a066e4fdd01a76bc4cc
Juliane Marubayashi <up201800175@fe.up.pt>
2021-06-03 11:36:23 +0100

    changed report route

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,7 @@ Route::post('/api/user/{id}/notification', 'NotificationController@read');
 Route::delete('/api/user/{id}/notification', 'NotificationController@delete');
 
 // Report
-Route::post('api/report/user/{id}', 'ReportController@reportUser');
+Route::post('api/user/{id}/report', 'ReportController@reportUser');
 
 // MODULE 03 - Questions -----------------------------------------------------------------------------
 // Search Questions
